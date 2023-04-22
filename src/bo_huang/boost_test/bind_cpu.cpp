d6677d586d9eef9ad3a1add21600d8219ca9f044
bobozhezhe <bo.huang.2017@gmail.com>
2023-04-23 00:01:23 +0800

    update bind_cpu

--- src/bo_huang/boost_test/bind_cpu.cpp
+++ src/bo_huang/boost_test/bind_cpu.cpp
@@ -1,22 +1,34 @@
-#include <sched.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <sched.h>
+#include <numa.h>
+#include <unistd.h>
 
-int main()
-{
+int main(int argc, char** argv) {
+    int rank, size;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int ncores = sysconf(_SC_NPROCESSORS_CONF);
     cpu_set_t cpu_mask;
     CPU_ZERO(&cpu_mask);
-    CPU_SET(0, &cpu_mask); // 设置进程绑定到CPU0
+    CPU_SET(rank % ncores, &cpu_mask); // 绑定到当前进程rank对应的CPU核心上
 
     if (sched_setaffinity(0, sizeof(cpu_set_t), &cpu_mask) != 0) {
         perror("sched_setaffinity");
-        return -1;
+        MPI_Abort(MPI_COMM_WORLD, -1);
     }
 
-    // 打印当前进程绑定的CPU编号
-    printf("Process is running on CPU %d\n", sched_getcpu());
+    int cpu = sched_getcpu();
+    int node = numa_node_of_cpu(cpu);
+    printf("Process %d is running on CPU %d", rank, cpu);
+    std::cout << " on Node " << node << std::endl;
 
-    // 其他代码
+
+    MPI_Finalize();
+
     return 0;
 }
-
