b695b6160a53a1a1315ef475cdd7f5f8b0c38a84
bobozhezhe <bo.huang.2017@gmail.com>
2023-04-23 13:40:59 +0800

    got sll shm data.

--- src/bo_huang/singly_linked_list/sll_cross.cpp
+++ src/bo_huang/singly_linked_list/sll_cross.cpp
@@ -5,9 +5,13 @@
 #include <forward_list>
 #include <fstream>
 #include <typeinfo>
-#include <type_traits>
-#include <list>
+#include <boost/interprocess/managed_shared_memory.hpp>
+#include <boost/interprocess/allocators/allocator.hpp>
 #include <boost/container/slist.hpp>
+#include <boost/mpi.hpp>
+namespace bip = boost::interprocess;
+
+const int MEM_LENGTH = 256 * (1 << 20);
 
 #define MEASURE_TIME_HIGH_RESOLUTION(loop_code)                                                                     \
     do                                                                                                              \
@@ -20,47 +24,47 @@
         times.push_back(duration_emplace.count());                                                                  \
     } while (0)
 
-// Define the linked list node
-class ListNode : public boost::intrusive::slist_base_hook<>
-{
-public:
-    int val;
-    ListNode(int x) : val(x) {}
-};
-
-// Define the linked list type
-using List = boost::intrusive::slist<ListNode>;
 
 template <typename T>
-void test_boost_slist(int loop_num, std::vector<double> &times)
+void test_shm_slist(int loop_num, std::vector<double> &times)
 {
+    typedef bip::allocator<int, bip::managed_shared_memory::segment_manager> ShmemAllocator;
+    typedef boost::container::slist<int, ShmemAllocator> MySlist;
+    std::cout << "datatype = " << typeid(T).name() << std::endl;
 
-    List *arr = new List();
+    std::string shm_name = "my_shared_memory";
+    bip::shared_memory_object::remove(shm_name.c_str());
 
-    std::cout << "boost::unordered_map emplace ";
+    bip::managed_shared_memory segment(bip::create_only, shm_name.c_str(), MEM_LENGTH);
+    MySlist *slist = segment.construct<MySlist>("MySlist")(segment.get_segment_manager());
+
+    // Insertion performance test
+    std::cout << "boost::container::slist on shared_memory push_front ";
     MEASURE_TIME_HIGH_RESOLUTION(
-        for (int i = 0; i < loop_num; i++) {
-            ListNode *node_ptr = new ListNode{i};
-            arr->push_front(*node_ptr);
-        });
+        for (int i = 0; i < loop_num; ++i) {
+            slist->push_front(i);
+        }
+    );
 
-    int value = 0;
-    std::cout << "boost::unordered_map read ";
+    // Read performance test
+    std::cout << "boost::container::slist on shared_memory read ";
     MEASURE_TIME_HIGH_RESOLUTION(
-        for (auto node
-             : *arr) {
-            value = node.val;
-        });
+        for (auto it = slist->begin(); it != slist->end(); ++it) {
+            int value = *it;
+        }
+    );
 
-    std::cout << "boost::slist erase ";
+    // Deletion performance test
+    std::cout << "boost::container::slist on shared_memory erase ";
     MEASURE_TIME_HIGH_RESOLUTION(
-        for (auto it = arr->begin(); it != arr->end(); ) {
-            it = arr->erase(it);
-        });
+        for (auto it = slist->begin(); it != slist->end(); ) {
+            it = slist->erase(it);
+        }
+    );
 
-    delete arr;
-
+    segment.destroy<MySlist>("MySlist");
 }
+
 
 int main()
 {
@@ -69,7 +73,7 @@ int main()
 
     const int kNumTests = 4;
     constexpr int kNumIters[kNumTests] = {1000, 10000, 100000, 1000000};
-    std::ofstream file("boost_results.csv");
+    std::ofstream file("shm_results.csv");
 
     // test
     int row = 0;
@@ -78,18 +82,15 @@ int main()
     for (int i = 0; i < kNumTests; i++)
     {
         int loop_num = kNumIters[i];
-        // test_std_forward_list<int>(loop_num);
-        // }
+        std::cout << "boost::container::slist on shared_memory loop = " << loop_num << std::endl;
 
-        // 测试 std::forward_list
-        // std::cout << "std::forward_list<int> test:" << std::endl;
-        test_boost_slist<int>(loop_num, times);
-        test_boost_slist<double>(loop_num, times);
-        test_boost_slist<std::vector<int>>(loop_num, times);
+        test_shm_slist <int>(loop_num, times);
+        test_shm_slist <double>(loop_num, times);
+        test_shm_slist <std::vector<int>>(loop_num, times);
 
         std::string container_names[1] = {"boost::intrusive::slist"};
         std::string datatype_names[3] = {typeid(int).name(), typeid(double).name(), typeid(std::vector<int>).name()};
-        std::string operation_names[3] = {"push_front", "read", "push_back/erase"};
+        std::string operation_names[3] = {"insert", "read", "erase"};
 
         for (int container_num = 0; container_num < 1; container_num++)
         {
